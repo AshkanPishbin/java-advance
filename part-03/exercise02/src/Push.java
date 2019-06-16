219ca04bba693ba276ba4d300f19efee87522fe2
Ashkan <ashkan.pb@gmail.com>
2019-06-16 15:26:13 +0200

    Push added

---
+++ part-03/exercise02/src/Push.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Push {
+    public List<String> push(List<String> elements, String element) {
+        elements.add(element);
+        return elements;
+    }
+}
